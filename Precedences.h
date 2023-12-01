6f3b33ba454abb489a56ce3f0d83f2313e3e84f2
ambrnsnssi <132473638+ambrnsnssi@users.noreply.github.com>
2023-12-01 18:37:20 +0100

    Update Precedences.h

--- Precedences.h
+++ Precedences.h
@@ -5,12 +5,5 @@
 #ifndef OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_2_10_PRECEDENCES_H
 #define OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_2_10_PRECEDENCES_H
 
-typedef struct precedence{
-    int sommet_d;
-    int sommet_f;
-}t_precedence;
-
-t_precedence* lirefichier_pr();
-void afficher_pr(t_precedence * prs, int num);
 
 #endif //OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_2_10_PRECEDENCES_H
