258e2660c06a1cc7a96e9e25b4e11df58817e437
Ranya STITOU <ranyast@yahoo.fr>
2023-12-03 09:42:42 +0100

    Modification du main

--- main.c
+++ main.c
@@ -3,32 +3,46 @@
 //
 
 #include <stdio.h>
-#include "Exclusion.h"
-#include "Operations.h"
-#include "Graphe.h"
-#include "Precedences.h"
-#include "Temps_de_cycle.h"
+#include "Header.h"
+
 
 int main() {
 
-    t_operation* ops = lirefichier_op();
+    int nombre_op;
+    int nombre_ex;
+    int nombre_prs;
+
+    t_operation* ops = lirefichier_op(&nombre_op);
     afficher_op(ops,31);
 
-    t_exclusion* exc = lirefichier_ex();
+    t_exclusion* exc = lirefichier_ex(&nombre_ex);
     afficher_ex(exc, 20);
 
-    t_graphe * g = initialisation_graphe(ops, exc);
+    t_graphe * g = initialisation_graphe(ops, exc, nombre_op, nombre_ex);
     int * couleur = coloration_graphe(g);
 
     for(int i = 0; i<31; i++){
         printf("Couleur de %d: %d\n", ops[i].num_operation, couleur[i]);
     }
 
-    t_precedence* prs = lirefichier_pr();
+    t_precedence* prs = lirefichier_pr(&nombre_prs);
     afficher_pr(prs, 34);
 
     t_temps_cycle * t = lirefichier_tmps();
     afficher_tmps(t, 1);
 
+    t_graphe * g2 = init_graphe(ops, prs, nombre_op, nombre_prs);
+    int pile_index = 0;
+    printf("%d\n", pile_index);
+    int * tri_topo = tri_topologique(g2, &pile_index);
+    printf("%d\n", pile_index);
+    for(int i = 1; i<pile_index; i++){
+        printf("la valeur numero %d du graphe est : %d\n", ops[i].num_operation, ops[tri_topo[i]].num_operation);
+    }
+
+
+
+
     return 0;
 }
+
